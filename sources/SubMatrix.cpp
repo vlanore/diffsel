41c9be22858094042159f178558bd6096342618b
Nicolas Lartillot <nicolas.lartillot@gmail.com>
2017-05-30 18:45:01 +0200

    linalg2 does not work yet...

--- sources/SubMatrix.cpp
+++ sources/SubMatrix.cpp
@@ -3,8 +3,8 @@
 #include <cstdlib>
 #include <fstream>
 #include <iostream>
-// #include "linalg.hpp"
-#include "linalg2.hpp"
+#include "linalg.hpp"
+// #include "linalg2.hpp"
 using namespace std;
 
 int SubMatrix::nuni = 0;
@@ -114,7 +114,7 @@ void SubMatrix::ScalarMul(double e) {
 // ---------------------------------------------------------------------------
 //     Diagonalise()
 // ---------------------------------------------------------------------------
-/*
+
 int SubMatrix::Diagonalise() const {
     if (!ArrayUpdated()) {
         UpdateMatrix();
@@ -134,8 +134,8 @@ int SubMatrix::Diagonalise() const {
     diagflag = true;
     return static_cast<int>(failed);
 }
-*/
 
+/*
 int SubMatrix::Diagonalise() const {
     if (!ArrayUpdated()) {
         UpdateMatrix();
@@ -152,7 +152,10 @@ int SubMatrix::Diagonalise() const {
 	int* iw = new int[Nstate];
 
 	// diagonalise a into v and u
+    cerr << "diag\n";
 	int success = EigenRealGeneral(Nstate, aux, v, vi, u, iw, w);
+    cerr << "diag ok\n";
+    cerr << success << '\n';
 
 	// copy u into aux
 	for (int i=0; i<Nstate; i++)	{
@@ -170,6 +173,7 @@ int SubMatrix::Diagonalise() const {
 	delete[] iw;
 	return success;
 }
+*/
 
 // ---------------------------------------------------------------------------
 //     ComputeRate()
