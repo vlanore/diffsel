48dd7e33300206924b1773a65ac8b710a7e0513e
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-06-29 15:05:11 +0200

    Massive rewriting in StateSpace + vectorification of BiologicalSequences

--- src/BiologicalSequences.hpp
+++ src/BiologicalSequences.hpp
@@ -1,116 +1,101 @@
 #ifndef BIOLOGICALSEQUENCES_H
 #define BIOLOGICALSEQUENCES_H
 
-#include <cstdlib>
 #include <iostream>
-#include <string>
+#include <vector>
 
 static const std::string Path = "";
 
 enum DataType { DNA = 0, RNA = 1, Protein = 2, Other = 3 };
 
 const int RYN = 2;
-const char RYset[] = {'R', 'Y'};
+const std::vector<char> RYset{'R', 'Y'};
 
-const char AAset[] = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
-                      'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'a', 'c', 'd', 'e', 'f', 'g',
-                      'h', 'i', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
-                      'y', '-', '?', '$', '.', 'B', 'Z', '*', 'X', 'x'};
+const std::vector<char> AAset{'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N', 'P',
+                              'Q', 'R', 'S', 'T', 'V', 'W', 'Y', 'a', 'c', 'd', 'e', 'f', 'g',
+                              'h', 'i', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w',
+                              'y', '-', '?', '$', '.', 'B', 'Z', '*', 'X', 'x'};
 const int AAN = 49;
 const int DNAN = 38;
 const int RNAN = 38;
-const char DNAset[] = {'A', 'C', 'G', 'T', 'a', 'c', 'g', 't', 'B', 'D', 'H', 'K', 'M',
-                       'N', 'R', 'S', 'V', 'W', 'Y', 'b', 'd', 'h', 'k', 'm', 'n', 'r',
-                       's', 'v', 'w', 'y', '-', '?', '$', '.', '*', 'X', 'x'};
-const char RNAset[] = {'A', 'C', 'G', 'U', 'a', 'c', 'g', 'u', 'B', 'D', 'H', 'K', 'M',
-                       'N', 'R', 'S', 'V', 'W', 'Y', 'b', 'd', 'h', 'k', 'm', 'n', 'r',
-                       's', 'v', 'w', 'y', '-', '?', '$', '.', '*', 'X', 'x'};
+const std::vector<char> DNAset{'A', 'C', 'G', 'T', 'a', 'c', 'g', 't', 'B', 'D', 'H', 'K', 'M',
+                               'N', 'R', 'S', 'V', 'W', 'Y', 'b', 'd', 'h', 'k', 'm', 'n', 'r',
+                               's', 'v', 'w', 'y', '-', '?', '$', '.', '*', 'X', 'x'};
+const std::vector<char> RNAset{'A', 'C', 'G', 'U', 'a', 'c', 'g', 'u', 'B', 'D', 'H', 'K', 'M',
+                               'N', 'R', 'S', 'V', 'W', 'Y', 'b', 'd', 'h', 'k', 'm', 'n', 'r',
+                               's', 'v', 'w', 'y', '-', '?', '$', '.', '*', 'X', 'x'};
 
 // amino acids
 
 const int precision = 10000;
 const std::string Alphabet = "Amino_Acids";
-const char AminoAcids[] = {'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
-                           'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '-'};
-const char aminoacids[] = {'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm',
-                           'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', '-'};
-const char RYletters[] = {'R', 'Y'};
-const char DNAletters[] = {'A', 'C', 'G', 'T'};
-const char dnaletters[] = {'a', 'c', 'g', 't'};
-const char RNAletters[] = {'A', 'C', 'G', 'U'};
-const char rnaletters[] = {'a', 'c', 'g', 'u'};
+const std::vector<char> AminoAcids{'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M',
+                                   'N', 'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y', '-'};
+const std::vector<char> aminoacids{'a', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'k', 'l', 'm',
+                                   'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'y', '-'};
+const std::vector<char> RYletters{'R', 'Y'};
+const std::vector<char> DNAletters{'A', 'C', 'G', 'T'};
+const std::vector<char> dnaletters{'a', 'c', 'g', 't'};
+const std::vector<char> RNAletters{'A', 'C', 'G', 'U'};
+const std::vector<char> rnaletters{'a', 'c', 'g', 'u'};
 
-const int Dayhoff6Table[] = {3, 5, 2, 2, 4, 3, 1, 0, 1, 0, 0, 2, 3, 2, 1, 3, 3, 0, 4, 4};
-const int Dayhoff4Table[] = {3, -1, 2, 2, 0, 3, 1, 0, 1, 0, 0, 2, 3, 2, 1, 3, 3, 0, 2, 2};
+const std::vector<int> Dayhoff6Table{3, 5, 2, 2, 4, 3, 1, 0, 1, 0, 0, 2, 3, 2, 1, 3, 3, 0, 4, 4};
+const std::vector<int> Dayhoff4Table{3, -1, 2, 2, 0, 3, 1, 0, 1, 0, 0, 2, 3, 2, 1, 3, 3, 0, 2, 2};
 
 const int unknown = -1;
 
 const int Naa = 20;
 const int Nnuc = 4;
 const int Ncodon = 64;
-const std::string Codons[] = {
+const std::vector<std::string> Codons{
     "TTT", "TTC", "TTA", "TTG", "TCT", "TCC", "TCA", "TCG", "TAT", "TAC", "TAA", "TAG", "TGT",
     "TGC", "TGA", "TGG", "CTT", "CTC", "CTA", "CTG", "CCT", "CCC", "CCA", "CCG", "CAT", "CAC",
     "CAA", "CAG", "CGT", "CGC", "CGA", "CGG", "ATT", "ATC", "ATA", "ATG", "ACT", "ACC", "ACA",
     "ACG", "AAT", "AAC", "AAA", "AAG", "AGT", "AGC", "AGA", "AGG", "GTT", "GTC", "GTA", "GTG",
     "GCT", "GCC", "GCA", "GCG", "GAT", "GAC", "GAA", "GAG", "GGT", "GGC", "GGA", "GGG"};
-const int codonpos[][64] = {{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1,
-                             1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-                             0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
-                            {3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1,
-                             1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0,
-                             2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2},
-                            {3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1,
-                             0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2,
-                             3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2}};
+const int codonpos[][64]{{3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1,
+                          1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+                          0, 0, 0, 0, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2},
+                         {3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1,
+                          1, 1, 0, 0, 0, 0, 2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0,
+                          2, 2, 2, 2, 3, 3, 3, 3, 1, 1, 1, 1, 0, 0, 0, 0, 2, 2, 2, 2},
+                         {3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1,
+                          0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2,
+                          3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2, 3, 1, 0, 2}};
 
 enum GeneticCodeType { Universal = 0, MtMam = 1, MtInv = 2, MtProt = 3, MtEch = 4 };
 // universal genetic code
-// const std::string UniStopCodons[] = {"TAA","TAG","TGA"};
+// const std::string UniStopCodons[]  {"TAA","TAG","TGA"};
 const int UniNStopCodons = 3;
-const int UniStopCodons[] = {10, 11, 14};
-int const UniCodonCode[] = {4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  -1, 18,
-                            9,  9,  9,  9,  12, 12, 12, 12, 6,  6,  13, 13, 14, 14, 14, 14,
-                            7,  7,  7,  10, 16, 16, 16, 16, 11, 11, 8,  8,  15, 15, 14, 14,
-                            17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
-const int UniStopPos1[] = {3, 3, 3};
-const int UniStopPos2[] = {0, 0, 2};
-const int UniStopPos3[] = {0, 2, 0};
+const std::vector<int> UniStopCodons{10, 11, 14};
+const std::vector<int> UniCodonCode{4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  -1, 18,
+                                    9,  9,  9,  9,  12, 12, 12, 12, 6,  6,  13, 13, 14, 14, 14, 14,
+                                    7,  7,  7,  10, 16, 16, 16, 16, 11, 11, 8,  8,  15, 15, 14, 14,
+                                    17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
+const std::vector<int> UniStopPos1{3, 3, 3};
+const std::vector<int> UniStopPos2{0, 0, 2};
+const std::vector<int> UniStopPos3{0, 2, 0};
 
 const int MtInvNStopCodons = 2;
-const int MtInvStopCodons[] = {10, 11};
-int const MtInvCodonCode[] = {4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  18, 18,
-                              9,  9,  9,  9,  12, 12, 12, 12, 6,  6,  13, 13, 14, 14, 14, 14,
-                              7,  7,  10, 10, 16, 16, 16, 16, 11, 11, 8,  8,  15, 15, 15, 15,
-                              17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
-const int MtInvStopPos1[] = {3, 3};
-const int MtInvStopPos2[] = {0, 0};
-const int MtInvStopPos3[] = {0, 2};
+const std::vector<int> MtInvStopCodons{10, 11};
+const std::vector<int> MtInvCodonCode{
+    4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  18, 18, 9,  9,  9,  9,  12, 12,
+    12, 12, 6,  6,  13, 13, 14, 14, 14, 14, 7,  7,  10, 10, 16, 16, 16, 16, 11, 11, 8,  8,
+    15, 15, 15, 15, 17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
+const std::vector<int> MtInvStopPos1{3, 3};
+const std::vector<int> MtInvStopPos2{0, 0};
+const std::vector<int> MtInvStopPos3{0, 2};
 
 // mammal mitochondrial genetic code
 const int MtMamNStopCodons = 4;
-const int MtMamStopCodons[] = {10, 11, 46, 47};
-int const MtMamCodonCode[] = {4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  18, 18,
-                              9,  9,  9,  9,  12, 12, 12, 12, 6,  6,  13, 13, 14, 14, 14, 14,
-                              7,  7,  10, 10, 16, 16, 16, 16, 11, 11, 8,  8,  15, 15, -1, -1,
-                              17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
-const int MtMamStopPos1[] = {3, 3, 0, 0};
-const int MtMamStopPos2[] = {0, 0, 2, 2};
-const int MtMamStopPos3[] = {0, 2, 0, 2};
-
-/*
-// Protozoan and Coelenterate mitochondrial genetic code
-const int MtProtNStopCodons = 2;
-const int MtProtStopCodons[] = {10,11};
-int const MtProtCodonCode[] =
-{4,4,9,9,15,15,15,15,19,19,-1,-1,1,1,18,18,9,9,9,9,12,12,12,12,6,6,13,13,14,14,14,14,7,7,7,10,16,16,16,16,11,11,8,8,15,15,14,14,17,17,17,17,0,0,0,0,2,2,3,3,5,5,5,5};
-
-// Echinoderm and Flatworm mitochondrial genetic code
-const int MtEchNStopCodons = 2;
-const int MtEchStopCodons[] = {10,11};
-int const MtEchCodonCode[] =
-{4,4,9,9,15,15,15,15,19,19,-1,-1,1,1,18,18,9,9,9,9,12,12,12,12,6,6,13,13,14,14,14,14,7,7,7,10,16,16,16,16,11,11,11,8,15,15,15,15,17,17,17,17,0,0,0,0,2,2,3,3,5,5,5,5};
-*/
+const std::vector<int> MtMamStopCodons{10, 11, 46, 47};
+const std::vector<int> MtMamCodonCode{
+    4,  4,  9,  9,  15, 15, 15, 15, 19, 19, -1, -1, 1,  1,  18, 18, 9,  9,  9,  9,  12, 12,
+    12, 12, 6,  6,  13, 13, 14, 14, 14, 14, 7,  7,  10, 10, 16, 16, 16, 16, 11, 11, 8,  8,
+    15, 15, -1, -1, 17, 17, 17, 17, 0,  0,  0,  0,  2,  2,  3,  3,  5,  5,  5,  5};
+const std::vector<int> MtMamStopPos1{3, 3, 0, 0};
+const std::vector<int> MtMamStopPos2{0, 0, 2, 2};
+const std::vector<int> MtMamStopPos3{0, 2, 0, 2};
 
 inline std::istream &operator>>(std::istream &is, GeneticCodeType &type) {
     std::string t;
--- src/StateSpace.hpp
+++ src/StateSpace.hpp
@@ -1,7 +1,10 @@
 #ifndef STATESPACE_H
 #define STATESPACE_H
 
+#include <doctest.h>
+#include <algorithm>
 #include <string>
+#include <vector>
 #include "BiologicalSequences.hpp"  //FIXME only here because constant unknown
 
 class StateSpace {
@@ -10,7 +13,6 @@ class StateSpace {
 
     virtual std::string GetState(int state) const = 0;
     virtual int GetNstate() const = 0;
-
     virtual int GetState(std::string from) const = 0;
 
     virtual bool isCompatible(int state1, int state2) const {
@@ -18,69 +20,82 @@ class StateSpace {
     }
 };
 
-// simple state space: assumes that states are referred to using a one-letter
-// code
+// simple state space: assumes that states are referred to using a one-letter code
 class SimpleStateSpace : public StateSpace {
   public:
-    virtual ~SimpleStateSpace() {}
-    int GetState(std::string from) const override;
+    SimpleStateSpace(const std::vector<char> &alphabet, const std::vector<char> &alphabetSet)
+        : Alphabet(alphabet), AlphabetSet(alphabetSet) {}
 
-    int GetNstate() const override { return Nstate; }
+    int GetState(std::string from) const override {
+        if (from.length() != 1) {
+            std::cerr << "error in SimpleStateSpace\n";
+            exit(1);
+        }
+        char c = from[0];
+        auto pos = std::find(AlphabetSet.begin(), AlphabetSet.end(), c);
+        if (pos == AlphabetSet.end()) {
+            std::cerr << "error: does not recognise character " << c << '\n';
+            exit(1);
+        }
+        int index = pos - AlphabetSet.begin();
+        if (index >= 2 * GetNstate()) {
+            return unknown;
+        } else {
+            return index % GetNstate();
+        }
+    }
 
-    std::string GetState(int state) const override;
+    int GetNstate() const override { return Alphabet.size(); }
+    std::string GetState(int state) const override {
+        if (state == unknown) {
+            return "-";
+        } else {
+            return std::string(1, Alphabet.at(state));
+        }
+    }
 
   protected:
-    int Nstate;
-    char *Alphabet;
-    int NAlphabetSet;
-    char *AlphabetSet;
+    const std::vector<char> &Alphabet;
+    const std::vector<char> &AlphabetSet;
 };
 
 class DNAStateSpace : public SimpleStateSpace {
   public:
-    DNAStateSpace();
-    ~DNAStateSpace() override;
+    DNAStateSpace(const std::vector<char> &alphabet = DNAletters,
+                  const std::vector<char> &alphabetSet = DNAset)
+        : SimpleStateSpace(alphabet, alphabetSet) {}
 };
 
 class RNAStateSpace : public SimpleStateSpace {
   public:
-    RNAStateSpace();
-    ~RNAStateSpace() override;
+    RNAStateSpace(const std::vector<char> &alphabet = RNAletters,
+                  const std::vector<char> &alphabetSet = RNAset)
+        : SimpleStateSpace(alphabet, alphabetSet) {}
 };
 
 class ProteinStateSpace : public SimpleStateSpace {
   public:
-    ProteinStateSpace();
-    ~ProteinStateSpace() override;
+    ProteinStateSpace(const std::vector<char> &alphabet = AminoAcids,
+                      const std::vector<char> &alphabetSet = AAset)
+        : SimpleStateSpace(alphabet, alphabetSet) {}
 };
 
-class RYStateSpace : public SimpleStateSpace {
-  public:
-    RYStateSpace();
-    ~RYStateSpace() override;
-
-    int GetRYCoding(int from);
-};
-
-class GenericStateSpace : public SimpleStateSpace {
-  public:
-    GenericStateSpace(int inNstate, char *inAlphabet, int inNAlphabetSet, char *inAlphabetSet) {
-        Nstate = inNstate;
-        Alphabet = new char[Nstate];
-        for (int i = 0; i < Nstate; i++) {
-            Alphabet[i] = inAlphabet[i];
-        }
-        NAlphabetSet = inNAlphabetSet;
-        AlphabetSet = new char[NAlphabetSet];
-        for (int i = 0; i < NAlphabetSet; i++) {
-            AlphabetSet[i] = inAlphabetSet[i];
-        }
-    }
-
-    ~GenericStateSpace() override {
-        delete[] Alphabet;
-        delete[] AlphabetSet;
-    }
-};
+TEST_CASE("StateSpace tests") {
+    DNAStateSpace myDnaSS;
+    CHECK(myDnaSS.GetState(0) == "A");
+    CHECK(myDnaSS.GetState(1) == "C");
+    CHECK(myDnaSS.GetState(2) == "G");
+    CHECK(myDnaSS.GetState(3) == "T");
+    CHECK(myDnaSS.GetNstate() == 4);
+    CHECK(myDnaSS.GetState("A") == 0);
+    CHECK(myDnaSS.GetState("C") == 1);
+    CHECK(myDnaSS.GetState("G") == 2);
+    CHECK(myDnaSS.GetState("T") == 3);
+    CHECK(myDnaSS.GetState("a") == 0);
+    CHECK(myDnaSS.GetState("c") == 1);
+    CHECK(myDnaSS.GetState("g") == 2);
+    CHECK(myDnaSS.GetState("t") == 3);
+    CHECK(myDnaSS.GetState("*") == unknown);
+}
 
 #endif  // STATESPACE_H
