494186aca4b69c02e58519365e4fe1e762ba5671
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-10-09 16:43:17 +0200

    License: updated contributor list in files

--- src/Chain.cpp
+++ src/Chain.cpp
@@ -1,6 +1,7 @@
 /*Copyright or © or Copr. Centre National de la Recherche Scientifique (CNRS) (2017-06-14).
 Contributors:
 * Nicolas LARTILLOT - nicolas.lartillot@univ-lyon1.fr
+* Vincent LANORE - vincent.lanore@univ-lyon1.fr
 
 This software is a computer program whose purpose is to detect convergent evolution using Bayesian
 phylogenetic codon models.
