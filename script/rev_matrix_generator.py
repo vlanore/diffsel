b7f2fdbb87dc6c98f02809a41a5a53d7d9821817
Vincent Lanore <vincent.lanore@univ-lyon1.fr>
2017-10-20 10:22:01 +0200

    script: rev gen now uses base 64

--- script/rev_matrix_generator.py
+++ script/rev_matrix_generator.py
@@ -6,18 +6,20 @@ codon_table = dict(zip(codons, amino_acids))
 
 reverse_bases = dict(zip(bases, range(4)))
 short_aa_table = "FFLLSSSSYYCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
-#aas = "".join(sorted("FSYCWLPHQRIMTNKRVADEG")) # Je corrige, il y a 2 fois R...
-#reverse_aas = dict(zip(aas, range(21)))
-aas = "".join(sorted("FSYCWLPHQRIMTNKVADEG")) # Je corrige, il y a 2 fois R...
+# short_aa_table = "FFLLSSSSYYCCWLLLLPPPPHHQQRRRRIIIMTTTTNNKKSSRRVVVVAAAADDEEGGGG"
+aas = "".join(sorted("FSYCWLPHQRIMTNKVADEG"))
 reverse_aas = dict(zip(aas, range(20)))
 
 def cno_to_nuc(cno):
-    if (cno <10):
-        return codons[cno]
-    elif (cno == 10 or cno == 11):
-        return codons[cno+1]
-    else:
-        return codons[cno+2]
+    return codons[cno]
+
+# def cno_to_nuc(cno):
+#     if (cno <10):
+#         return codons[cno]
+#     elif (cno == 10 or cno == 11):
+#         return codons[cno+1]
+#     else:
+#         return codons[cno+2]
 
 def synonym(c1, c2):
     return codon_table[cno_to_nuc(c1)] == codon_table[cno_to_nuc(c2)]
@@ -31,10 +33,10 @@ def diff(cno1, cno2):
             r.append((c1[i], c2[i]))
     return r
 
-
 def case(c1, c2):
     d = diff(c1, c2)
     if (len(d) != 1):
+<<<<<<< b10459504dc9d172d322b53d09b6fcef0db5da23
         a = 1
         #print("\tQcodons[cod][%d][%d] := 0" % (c1, c2))
     elif synonym(c1, c2):
@@ -54,6 +56,21 @@ def print_all():
     print("for (cod in 1:nsites) {")
     for cno in range(61):
         for cno2 in range(61):
+=======
+        print("\tQcodons[cod][%d][%d] := 0" % (c1, c2))
+    elif synonym(c1, c2):
+        print("\tQcodons[cod][%d][%d] := Q[%d][%d]" % (c1+1, c2+1, reverse_bases[d[0][0]]+1, reverse_bases[d[0][1]]+1))
+    elif (amino_acids[c1] == '*' or amino_acids[c2] == '*'):
+        print("\tQcodons[cod][%d][%d] := 0" % (c1, c2))
+    else:
+        print("\tQcodons[cod][%d][%d] := Q[%d][%d] * sqrt(fitness[cod][%d]/fitness[cod][%d])" %
+              (c1+1, c2+1, reverse_bases[d[0][0]]+1, reverse_bases[d[0][1]]+1, reverse_aas[amino_acids[c2]]+1, reverse_aas[amino_acids[c1]]+1))
+
+def print_all():
+    print("for (cod in 1:nsites) {")
+    for cno in range(64):
+        for cno2 in range(64):
+>>>>>>> script: rev gen now uses base 64
             case(cno, cno2)
     print("\tR[cod] := fnFreeK(Qcodons[cod])")
     print("}")
